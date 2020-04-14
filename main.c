2b81515f36e4a2a713d90d450ee016579372e2f9
Akira <54892411+docxed@users.noreply.github.com>
2020-04-14 17:28:21 +0700

    committed

--- main.c
+++ main.c
@@ -2,7 +2,34 @@
 #include <stdlib.h>
 
 void compare(){
-    printf("DEMO");
+    printf("CALVALUE\n\n");
+    printf("Compare the worthiness of products in different sizes, for example having to buy the same detergent pack When buying large packages, is it worth more than small packages?\n\n");
+
+    //input section
+    int amount;
+    printf("Amount of products: ");
+    scanf("%d", &amount);
+
+    //size and price input
+    printf("\nEnter size (gram/kilo/liter/...) and price (bath)\n\n");
+    float products[amount], price[amount], result[amount];
+    for(int i=0; i<amount; ++i){
+        printf("%d. Size: ", i+1);
+        scanf("%f", &products[i]);
+        printf("  Price: ");
+        scanf("%f", &price[i]);
+        //optimize process
+        result[i] = products[i]/price[i];
+    }
+
+    //find maximum
+
+
+    //output
+    for (int i=0; i<amount; ++i){
+        printf("%.2f\n", result[i]);
+    }
+
     return;
 }
 
@@ -10,7 +37,6 @@ int main()
 {
     int menu;
     printf("CALVALUE\n\n");
-
     printf("1. Compare the value of the product.\n");
     printf("0. Exit\n");
     printf("Enter number to continue\n");
@@ -19,6 +45,7 @@ int main()
     if(menu == 0){
         exit(0);
     }else if(menu == 1){
+        system("cls");
         compare();
     }else{
         printf("Please enter the correct number.\n");
